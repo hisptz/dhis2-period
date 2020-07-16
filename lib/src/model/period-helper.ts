32f9d8c85f08019232e39a719d43a2626592a347
Rajab Mkomwa <rajeyw@gmail.com>
2020-07-16 23:12:30 +0300

    refactor: replace string periodo type in deduce type with period type enums

--- lib/src/model/period-helper.ts
+++ lib/src/model/period-helper.ts
@@ -1,4 +1,4 @@
-import { parse } from 'yargs';
+import { PeriodTypeEnum } from '../constants/period-types.constant';
 
 export class PeriodHelper {
   static deduceTypeFromISO(iso: string): string {
@@ -7,42 +7,44 @@ export class PeriodHelper {
     const numberLikePeriod = parseInt(iso, 10);
     if (!isNaN(numberLikePeriod)) {
       if (iso.length === 4) {
-        periodType = 'Yearly';
+        periodType = PeriodTypeEnum.YEARLY;
       } else if (iso.indexOf('B') !== -1) {
-        periodType = 'BiMonthly';
+        periodType = PeriodTypeEnum.BI_MONTHLY;
       } else if (iso.indexOf('Q') !== -1) {
-        periodType = 'Quarterly';
+        periodType = PeriodTypeEnum.QUARTERLY;
       } else if (iso.indexOf('AprilS') !== -1) {
-        periodType = 'SixMonthlyApril';
+        periodType = PeriodTypeEnum.SIX_MONTHLY_APRIL;
       } else if (iso.indexOf('April') !== -1) {
-        periodType = 'FinancialApril';
+        periodType = PeriodTypeEnum.FINANCIAL_APRIL;
       } else if (iso.indexOf('S') !== -1) {
-        periodType = 'SixMonthly';
+        periodType = PeriodTypeEnum.SIX_MONTHLY;
       } else if (iso.indexOf('July') !== -1) {
-        periodType = 'FinancialJuly';
+        periodType = PeriodTypeEnum.FINANCIAL_JULY;
       } else if (iso.indexOf('Oct') !== -1) {
-        periodType = 'FinancialOctober';
+        periodType = PeriodTypeEnum.FINANCIAL_OCTOBER;
+      } else if (iso.indexOf('Nov') !== -1) {
+        periodType = PeriodTypeEnum.FINANCIAL_NOVEMBER;
       } else if (!isNaN(parseInt(iso.slice(5), 10))) {
-        periodType = 'Monthly';
+        periodType = PeriodTypeEnum.MONTHLY;
       }
     } else {
       if (iso.indexOf('QUARTER') !== -1) {
-        periodType = 'RelativeQuarter';
+        periodType = PeriodTypeEnum.RELATIVE_QUARTER;
       } else if (iso.indexOf('WEEK') !== -1) {
-        periodType = 'RelativeWeek';
+        periodType = PeriodTypeEnum.RELATIVE_WEEK;
       } else if (iso.indexOf('MONTH') !== -1) {
         if (iso.indexOf('BIMONTH') !== -1) {
-          periodType = 'RelativeBiMonth';
+          periodType = PeriodTypeEnum.RELATIVE_BI_MONTH;
         } else if (iso.indexOf('SIX_MONTH') !== -1) {
-          periodType = 'RelativeSixMonth';
+          periodType = PeriodTypeEnum.RELATIVE_SIX_MONTH;
         } else {
-          periodType = 'RelativeMonth';
+          periodType = PeriodTypeEnum.RELATIVE_MONTH;
         }
       } else if (iso.indexOf('YEAR') !== -1) {
         if (iso.indexOf('FINANCIAL_YEAR') !== -1) {
-          periodType = 'RelativeFinancialYear';
+          periodType = PeriodTypeEnum.RELATIVE_FINANCIAL_YEAR;
         } else {
-          periodType = 'RelativeYear';
+          periodType = PeriodTypeEnum.RELATIVE_YEAR;
         }
       }
     }
